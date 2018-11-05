6b8dad7bac18ffa3e8b00705b46ed2dcf4f4c19b
Josh Butler <joshua.butler@my.scitexas.edu>
2018-11-05 15:58:31 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// jquery HOVER event for icons
+$(document).ready(function(){
+    $(".social-icons").hover(function(){
+        $(this).css("font-size", "150%");
+    }, function(){
+        $(this).css("font-size", "100%");
+    });
+});
+
+// AJAX call
+let gitHubRequest = new XMLHttpRequest();
+gitHubRequest.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+    let gitObject = JSON.parse(this.responseText);
+    let txt = "";
+    for(i = 0; i < gitObject.length; i++){
+        txt = txt + gitObject[i].name + "<br>"; 
+    }
+    document.getElementById("gitRepos").innerHTML = txt;
+}
+};
+
+gitHubRequest.open("GET", "https://api.github.com/users/joshorion7/repos", true);
+gitHubRequest.send();
